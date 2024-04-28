25c3d7bd52b62398e89c5d3fa0dcea69ec1cf455
Annonym Keinname <IpGa13>
2024-04-28 22:58:36 +0200

    Added blocks to Natural Creative Mode Tab

--- src/main/java/net/fredeun/randstuff/block/ModBlocks.java
+++ src/main/java/net/fredeun/randstuff/block/ModBlocks.java
@@ -1,12 +1,15 @@
 package net.fredeun.randstuff.block;
 
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.fredeun.randstuff.CreateRandomFunStuff;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
 import net.minecraft.item.BlockItem;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.item.Items;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
@@ -31,5 +34,13 @@ public class ModBlocks {
 
     public static void registerModBlocks() {
         CreateRandomFunStuff.LOGGER.info("Registering Blocks for Mod: " + CreateRandomFunStuff.MOD_ID);
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.NATURAL).register(content -> {
+            content.addAfter(Items.SMOOTH_BASALT, GROUND_BASALT);
+        });
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.NATURAL).register(content -> {
+            content.addAfter(Items.OBSIDIAN, GLOWING_OBSIDIAN);
+        });
     }
+
+
 }
